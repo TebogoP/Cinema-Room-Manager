9de79dee84df7e9350287e52097f0f8ccded9dca
TebogoP <33540200+TebogoP@users.noreply.github.com>
2024-11-23 20:50:18 +0200

    Initial Work on project.  Stage 1/5:Print seats  Stage 2/5:Calculate the profit

--- Cinema.java
+++ Cinema.java
@@ -2,9 +2,27 @@ package cinema;
 import java.util.*;
 
 public class Cinema {
+    // Work on project. Stage 2/5:Calculate the profit
+    public static void TotalInc(int numOfSeatsPerRow,int numOfRows ){
+        
+        int totalSeats = numOfSeatsPerRow * numOfRows;
+        int frontHalfRow = numOfRows/2;
+        int backHalfRow =numOfRows - frontHalfRow;
+        int totalCost=0;
+        
+        if (totalSeats<=60){
+            totalCost = totalSeats * 10;
+        }else{
+            totalCost = ((frontHalfRow * numOfSeatsPerRow) *10) + ((backHalfRow * numOfSeatsPerRow) *8);
+        }
+        System.out.print("Total income:\n$"+ totalCost);
+        
+    }
 
     public static void main(String[] args) {
         // Write your code here
+
+        //Work on project. Stage 1/5:Print seats
         String [][] floor = new String [9][9];
         int nRow = 1;
         int nCol = 1;
@@ -30,6 +48,15 @@ public class Cinema {
             }
             System.out.println();
         }
-        //System.out.println(floor.length);
+
+        // Work on project. Stage 2/5:Calculate the profit
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int numOfRows = sc.nextInt();
+        
+        System.out.println("Enter the number of seats in each row:");
+        int numOfSeatsPerRow = sc.nextInt();
+        
+        TotalInc(numOfSeatsPerRow,numOfRows );    
     }
 }
